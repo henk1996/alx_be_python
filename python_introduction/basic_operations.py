06be7bcd16c349bcfb7a3c4cf2eba4bd97e17325
henk1996 <henockmengistu@gmail.com>
2025-05-24 03:08:54 +0400

    update on arthimatic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number2 + number1
+difference = number2 - number1
+product = number2 * number1
+
+print("Addition of ", number1, " and ", number2, " is", sum)
+print("Substraction of ", number2 ,"and", number1 ,"is", difference)
+print("Multiplication of ", number1 ,"and", number2 ,"is", product)
