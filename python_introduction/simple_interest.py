439546635f570c21567bcd4207b1094ce611c314
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-05-24 16:20:59 +0400

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,10 @@
+#assigning principal,rate,time values
 principal = 1000 
 rate = 0.05
 time = 3 
 
+#calculating for interest
 interest = principal * rate * time
 
+#printing out the value of interest
 print ("The simple interest is :", interest)
