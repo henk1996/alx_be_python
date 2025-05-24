c6c95cc1d77a9f84882c4b814ba3fde186b06f45
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-05-24 16:16:21 +0400

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,13 @@
+#two variable assignment
 number1 = 10
 number2 = 5
 
+#performing Artimethic operations
 sum = number2 + number1
 difference = number2 - number1
 product = number2 * number1
 
+#displaying the out result
 print("Addition of ", number1, " and ", number2, " is", sum)
 print("Substraction of ", number2 ,"and", number1 ,"is", difference)
 print("Multiplication of ", number1 ,"and", number2 ,"is", product)
