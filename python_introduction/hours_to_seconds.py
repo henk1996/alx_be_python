836ba53197a8d7c7c9b1cc02101a59dd7383616c
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-05-24 16:18:52 +0400

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,8 @@
+#hours variable value assignement
 hours = 2
 
+#calculating second with in the hour
 seconds = 3600 * hours
 
+#displaying the result
 print (hours,"hours(s) is",seconds,"seconds.")
