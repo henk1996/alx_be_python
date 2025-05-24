84c776a93134dff997daa8d081206dd11c4de82c
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-05-24 05:17:22 +0400

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,8 +8,8 @@ projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 if monthly_savings == int(monthly_savings):
     print (f"Your monthly savings are ${int(monthly_savings)}.")
 else:
-   print (f"Your monthly savings are ${monthly_savings)}")
-if project_saving == int(projected_savings):
+   print (f"Your monthly savings are ${monthly_savings}")
+if projected_savings == int(projected_savings):
     print (f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
 else:
    print (f"Projected savings after one year, with interest, is: ${projected_savings}.")
