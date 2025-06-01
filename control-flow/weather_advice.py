405d1dc003d0a9443f6373fba12b23bb43b859e9
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-06-01 12:27:02 +0400

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,11 +6,11 @@
 
 user = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if(user == "sunny"):
+if user == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif(user == "rainy"):
+elif user == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif(user == "cold"):
+elif user == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
