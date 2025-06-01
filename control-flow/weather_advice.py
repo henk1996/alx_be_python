e08eb83fddec7769375908a9d32e0796c87e9ceb
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-06-01 12:29:51 +0400

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,13 +4,13 @@
 #If the weather is “cold”, recommend: Make sure to wear a warm coat and a scarf.
 #Include an else statement that handles unexpected input by printing: Sorry, I don't have recommendations for this weather.
 
-weather_input = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather_input == 'sunny':
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_input == 'rainy':
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_input == 'cold':
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
