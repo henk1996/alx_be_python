6bbd9db3cb206dd8f0b4efcc56814ff63ef8ba2a
henk1996 <121497550+henk1996@users.noreply.github.com>
2025-06-08 21:10:31 +0400

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -16,7 +16,7 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     elif operation == "divide":
            if num2 == 0:
-            return "Can't Divide By Zero"
+            return "Can't Divide By Zero" 
 <<<<<<< HEAD
            else:
             return num1 / num2
